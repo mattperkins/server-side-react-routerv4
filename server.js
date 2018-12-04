7431458a5eba9344785d133ed0cf180babe0e226
mattperkins <github@mattperkins.com>
2018-12-04 20:23:36 +0000

    format to standard

--- server.js
+++ server.js
@@ -5,41 +5,41 @@ import ReactDOMServer from 'react-dom/server'
 import { StaticRouter } from 'react-router'
 import App from './src/App'
 import bodyParser from 'body-parser'
-import {Helmet} from'react-helmet'
+import { Helmet } from 'react-helmet'
 
 const app = express()
-const PORT = process.env.PORT || 3000 
+const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(express.static('build/public'))
 
 app.get('*', (req, res) => {
- const context = {}
- const content = ReactDOMServer.renderToString(
-  <StaticRouter location={req.url} context={context}>
-   <App />
-  </StaticRouter>
- )
+  const context = {}
+  const content = ReactDOMServer.renderToString(
+    <StaticRouter location={req.url} context={context}>
+      <App />
+    </StaticRouter>
+  )
 
-const helmet = Helmet.renderStatic()
+  const helmet = Helmet.renderStatic()
 
- const html = `
- <html>
-   <head>
-     ${helmet.meta.toString()}
-     ${helmet.title.toString()}
-    <body>
-     <div id="root">
-       ${content} 
-     </div>
-     <script src="client_bundle.js"></script>
-    </body> 
-   </head> 
- </html> 
+  const html = `
+  <html>
+    <head>
+      ${helmet.meta.toString()}
+      ${helmet.title.toString()}
+      <body>
+      <div id="root">
+        ${content} 
+      </div>
+      <script src="client_bundle.js"></script>
+      </body> 
+    </head> 
+  </html> 
  `
- res.send(html) 
+  res.send(html)
 })
 
 app.listen(PORT, () => {
-console.log(`App running on ${PORT}`)
+  console.log(`App running on ${PORT}`)
 })
