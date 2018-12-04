87aa23512750f696e8110bd410bebac50fee4281
mattperkins <github@mattperkins.com>
2018-12-04 20:24:59 +0000

    format to standard

--- webpack.server.js
+++ webpack.server.js
@@ -5,30 +5,29 @@ module.exports = {
   target: 'node',
   entry: './server.js',
   output: {
-   filename: 'bundle.js',
-   path: path.resolve(__dirname, 'build'),
-   publicPath: '/build'
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/build'
   },
   module: {
-   rules: [
-    {
-     test: /\.js$/,
-     loader: 'babel-loader',
-     exclude: '/node_modules',
-     options: {
-      presets: [
-        'react',
-        'stage-0',
-        ['env', {
-         target: {
-          browsers: ['last 2 versions']
-         }
-        }]
-      ]
-     }
-    }
-   ]
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules',
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            ['env', {
+              target: {
+                browsers: ['last 2 versions']
+              }
+            }]
+          ]
+        }
+      }
+    ]
   },
   externals: [webpackNodeExternals()]
 }
-
