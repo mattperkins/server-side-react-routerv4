1b348f640bc9ce9010187befb76ab1df2f16cd19
mattperkins <github@mattperkins.com>
2018-12-04 20:24:43 +0000

    import component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,30 +1,34 @@
-import * as React from "react";
-import {Helmet} from 'react-helmet'
+import * as React from 'react'
+import { Helmet } from 'react-helmet'
 
-class HomePage extends React.Component{
+import Text from './Text'
 
-clickHandler(){
- console.log('Yes it worked!')
+class HomePage extends React.Component {
+  clickHandler () {
+    console.log('Yes it worked!')
+  }
+
+  head () {
+    return (
+      <Helmet>
+        <meta />
+        <title>DORMshed</title>
+      </Helmet>
+    )
+  }
+
+  render () {
+    return (
+      <div>
+        {this.head()}
+        <h1>HomePage</h1>
+
+        <button onClick={() => this.clickHandler()}>Console log</button>
+
+        <Text />
+      </div>
+    )
+  }
 }
-
-head(){
-  return (
-    <Helmet>
-      <meta />
-      <title>DORMshed</title>
-    </Helmet>
-  )
-}
-
- render(){
- return(
-   <div>
-      {this.head()}
-    <h1>HomePage</h1>
-
-     <button onClick={() => this.clickHandler()}>Console log</button>
-    </div>
-  )}
- }
 
 export default HomePage
