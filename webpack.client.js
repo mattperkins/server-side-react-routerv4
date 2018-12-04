7b5882e8c9dbdb133b0677f99b778348596efb83
mattperkins <github@mattperkins.com>
2018-12-04 20:24:53 +0000

    format to standard

--- webpack.client.js
+++ webpack.client.js
@@ -4,29 +4,31 @@ module.exports = {
   target: 'node',
   entry: './src/client.js',
   output: {
-   filename: 'client_bundle.js',
-   path: path.resolve(__dirname, 'build/public'),
-   publicPath: '/build/public'
+    filename: 'client_bundle.js',
+    path: path.resolve(__dirname, 'build/public'),
+    publicPath: '/build/public'
   },
   module: {
-   rules: [
-    {
-     test: /\.js$/,
-     loader: 'babel-loader',
-     exclude: '/node_modules',
-     options: {
-      presets: [
-        'react',
-        'stage-0',
-        ['env', {
-         target: {
-          browsers: ['last 2 versions']
-         }
-        }]
-      ]
-     }
-    }
-   ]
-  },
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules',
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            [
+              'env',
+              {
+                target: {
+                  browsers: ['last 2 versions']
+                }
+              }
+            ]
+          ]
+        }
+      }
+    ]
+  }
 }
-
