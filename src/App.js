6ea2d66d7a2eb81807b558d9e48fc0dadf58fcdf
mattperkins <github@mattperkins.com>
2018-12-04 20:24:01 +0000

    format to standard

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import * as React from 'react'
 import { Switch, Route } from 'react-router'
 import HomePage from './pages/HomePage'
 
-class App extends React.Component{
- render(){
-  return(
-    <Switch>
-     <Route path="/" render={props => (
-       <HomePage {...props} />
-     )} />
-    </Switch>
-  )
- }
+class App extends React.Component {
+  render () {
+    return (
+      <Switch>
+        <Route path='/' render={props => (
+          <HomePage {...props} />
+        )} />
+      </Switch>
+    )
+  }
 }
 
 export default App
